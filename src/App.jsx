897c870f80a3a9444780c67630f7d04eae915df0
Tyler <tylerjmarcott@gmail.com>
2024-03-17 21:44:41 -0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-3xl">
+        testy test
+      </div>
+    </>
+  )
+}
+
+export default App
